5e44e51d6947bd8604cb1a7a8fc37531aa3e891a
Nelson Chan <chakflying@hotmail.com>
2021-10-30 02:56:59 +0800

    Feat: Write data to sqlite database

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ extern crate gpx;
 use std::fs;
 use std::sync::mpsc;
 use std::thread;
-use std::time::{Instant};
+use std::time::Instant;
 
 use geo_types::Point;
 use gpx::{Fix, Track, TrackSegment, Waypoint};
@@ -15,11 +15,44 @@ use adafruit_gps::gga;
 use adafruit_gps::NmeaOutput;
 use adafruit_gps::{Gps, GpsSentence};
 
+use rusqlite::{Connection, Result};
+
+// Open and prepare Database
+fn open_db() -> Result<Connection> {
+    let db_path = "./data.db";
+    let db = Connection::open(&db_path)?;
+    db.pragma_update(None, "foreign_keys", "ON")
+        .expect("Failed to set PRAGMA");
+    db.pragma_update(None, "journal_mode", "WAL")
+        .expect("Failed to set PRAGMA");
+    db.pragma_update(None, "auto_vacuum", "FULL")
+        .expect("Failed to set PRAGMA");
+
+    db.execute(
+        "CREATE TABLE IF NOT EXISTS location_history (
+                  id              INTEGER PRIMARY KEY AUTOINCREMENT,
+                  waypoint        TEXT NOT NULL,
+                  createdAt       TEXT DEFAULT CURRENT_TIMESTAMP
+                  )",
+        [],
+    )
+    .expect("Failed when checking for history table in database");
+
+    Ok(db)
+}
+
 fn main() {
     let (tx, rx) = mpsc::channel::<TrackSegment>();
     let baud_rate = "115200";
     let port = "/dev/serial0";
+
+    // Store last update
     let mut last_update = Instant::now();
+
+    let mut last_speed = 0.0;
+    let mut last_speed_update = Instant::now();
+    let (mut pdop, mut vdop, mut hdop) = (0.0, 0.0, 0.0);
+    let mut last_dop_update = Instant::now();
 
     // Open the port that is connected to the GPS module.
     let mut gps = Gps::new(port, baud_rate);
@@ -36,6 +69,8 @@ fn main() {
     });
     let rate_result = gps.pmtk_220_set_nmea_updaterate("500");
     println!("Set update rate to 500ms: {:?}", rate_result);
+
+    let db = open_db().expect("Error connecting to database");
 
     // Store a segment in main thread.
     let mut segment = TrackSegment::new();
@@ -68,16 +103,17 @@ fn main() {
             GpsSentence::InvalidSentence => println!("Invalid sentence, try again"),
             GpsSentence::InvalidBytes => println!("Invalid bytes given, try again"),
             GpsSentence::NoConnection => println!("No connection with gps"),
+
             GpsSentence::GGA(sentence) => {
-                println!(
-                    "UTC: {}, Fix: {:?}\nLat: {}, Long: {}, Sats: {}, MSL Alt: {}",
-                    sentence.utc,
-                    (sentence.sat_fix != gga::SatFix::NoFix),
-                    sentence.lat.unwrap_or(0.0),
-                    sentence.long.unwrap_or(0.0),
-                    sentence.satellites_used,
-                    sentence.msl_alt.unwrap_or(0.0)
-                );
+                // println!(
+                //     "UTC: {}, Fix: {:?}\nLat: {}, Long: {}, Sats: {}, MSL Alt: {}",
+                //     sentence.utc,
+                //     (sentence.sat_fix != gga::SatFix::NoFix),
+                //     sentence.lat.unwrap_or(0.0),
+                //     sentence.long.unwrap_or(0.0),
+                //     sentence.satellites_used,
+                //     sentence.msl_alt.unwrap_or(0.0)
+                // );
                 if segment.points.len() > 200
                     || (segment.points.len() > 0 && last_update.elapsed().as_secs() > 3)
                 {
@@ -87,23 +123,56 @@ fn main() {
 
                 // Add point to GPX if there is a fix
                 if sentence.sat_fix != gga::SatFix::NoFix {
+                    // Create Waypoint and write data
                     let mut point = Waypoint::new(Point::new(
                         sentence.long.unwrap_or(0.0).into(),
                         sentence.lat.unwrap_or(0.0).into(),
                     ));
+
                     point.time = Some(Utc::now());
+
                     point.elevation = Some((sentence.msl_alt.unwrap_or(0.0)).into());
+
                     point.fix = if sentence.satellites_used == 3 {
                         Some(Fix::TwoDimensional)
                     } else {
                         Some(Fix::ThreeDimensional)
                     };
+
                     point.sat = Some(sentence.satellites_used as u64);
+
                     point.source = Some("MTK3339".into());
+
+                    if last_speed_update.elapsed().as_secs() < 3 {
+                        point.speed = Some(last_speed / 3.6);
+                    }
+
+                    if last_dop_update.elapsed().as_secs() < 3 {
+                        point.vdop = Some(vdop);
+                        point.hdop = Some(hdop);
+                        point.pdop = Some(pdop);
+                    }
+
+                    // Write to database
+                    let mut stmt = db
+                        .prepare_cached("INSERT INTO location_history (waypoint) VALUES (?)")
+                        .expect("Failed to prepare SQL statement");
+
+                    let save_result = stmt.execute([
+                        serde_json::to_string(&point).expect("Failed to serialize location")
+                    ]);
+                    match save_result {
+                        Err(e) => {
+                            println!("Error when saving to database: {:?}", e)
+                        }
+                        _ => {}
+                    }
+
                     segment.points.push(point);
                     last_update = Instant::now();
                 }
             }
+
             GpsSentence::GSA(sentence) => {
                 println!(
                     "PDOP: {}, VDOP:{}, HDOP:{}",
@@ -111,12 +180,20 @@ fn main() {
                     sentence.vdop.unwrap_or(0.0),
                     sentence.hdop.unwrap_or(0.0)
                 );
+
+                last_dop_update = Instant::now();
+
+                pdop = sentence.pdop.unwrap_or(0.0).into();
+                vdop = sentence.vdop.unwrap_or(0.0).into();
+                hdop = sentence.hdop.unwrap_or(0.0).into();
+
                 if let Some(last_point) = segment.points.last_mut() {
                     last_point.hdop = Some(sentence.hdop.unwrap_or(0.0).into());
                     last_point.vdop = Some(sentence.vdop.unwrap_or(0.0).into());
                     last_point.pdop = Some(sentence.pdop.unwrap_or(0.0).into());
                 }
             }
+
             GpsSentence::GSV(sentence) => {
                 println!(
                     "Signal Strength (dB): {:?}",
@@ -126,9 +203,13 @@ fn main() {
                         .collect::<Vec<_>>()
                 );
             }
+
             GpsSentence::VTG(sentence) => {
                 println!("Speed kph: {}", sentence.speed_kph.unwrap_or(0.0));
+                last_speed = sentence.speed_kph.unwrap_or(0.0).into();
+                last_speed_update = Instant::now();
             }
+
             _ => (),
         }
     }
